b658df39af6c4091b5f74f1f4a1fcbaa50e1b9a2
Risto C <ristosr@gmail.com>
2019-10-05 19:18:53 +0200

    add third junk method

--- OpenDota API Wrapper/Endpoints/PlayerEndpoint.cs
+++ OpenDota API Wrapper/Endpoints/PlayerEndpoint.cs
@@ -112,6 +112,11 @@ namespace OpenDotaDotNet.Endpoints
             return 1;
         }
 
+        public int ThirdJunkMethod()
+        {
+            return 1;
+        }
+
 
 
         /// <summary>
